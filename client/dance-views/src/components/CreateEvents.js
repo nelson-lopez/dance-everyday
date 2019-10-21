691b11b9f9f6eaa92ab7029cc96146fd8b8bd8e4
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-10-21 16:21:11 -0400

    added base functionality

--- client/dance-views/src/components/CreateEvents.js
+++ client/dance-views/src/components/CreateEvents.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const CreateEvents = () => {
+  const [currentInfo, setCurrentInfo] = useState('');
+
+  const handleOnInput = e => {
+    setCurrentInfo(e.target.input);
+  };
+
   return (
     <div>
       <form>
         <label>
           Create new event:
-          <input type="text" name="name" />
+          <input type="text" name="name" onChange={handleOnInput} />
         </label>
         <input type="submit" value="Submit" />
       </form>
