7b49f6035b5b0d08d1e54f6e35672734a9b9894f
Jaab <Jaabel@outlook.com>
2019-10-22 11:49:44 -0400

    merging

--- models.js
+++ models.js
@@ -1,25 +1,27 @@
-import { Model, DataTypes, Sequelize } from 'sequelize'
-export class Organizer extends Model { }
-export class Event extends Model { }
-export class Venue extends Model { }
-export class Ref_Dance_Style extends Model { }
+import { Model, DataTypes, Sequelize } from 'sequelize';
+export class Organizer extends Model {}
+export class Event extends Model {}
+export class Venue extends Model {}
+export class Ref_Dance_Style extends Model {}
 
-const sequelize = new Sequelize(
-    {
-        database: "dance_everyday_db",
-        dialect: "postgres"
-    })
+const sequelize = new Sequelize({
+  username: 'postgres',
+  password: 'dap',
+  database: 'dance_everyday_db',
+  dialect: 'postgres'
+});
 
 Organizer.init(
-    {
-        organizer_name: DataTypes.STRING,
-        company: DataTypes.STRING
-    },
-    {
-        freezeTableName: true,
-        tableName: "Organizer",
-        sequelize
-    })
+  {
+    organizer_name: DataTypes.STRING,
+    company: DataTypes.STRING
+  },
+  {
+    freezeTableName: true,
+    tableName: 'Organizer',
+    sequelize
+  }
+);
 
 Event.init(
     {
@@ -34,24 +36,26 @@ Event.init(
     })
 
 Venue.init(
-    {
-        venue_name: DataTypes.STRING,
-        venue_location: DataTypes.STRING,
-        venue_contact: DataTypes.STRING
-    },
-    {
-        freezeTableName: true,
-        tableName: "Venue",
-        sequelize
-    })
+  {
+    venue_name: DataTypes.STRING,
+    venue_location: DataTypes.STRING,
+    venue_contact: DataTypes.STRING
+  },
+  {
+    freezeTableName: true,
+    tableName: 'Venue',
+    sequelize
+  }
+);
 
 Ref_Dance_Style.init(
-    {
-        dance_style: DataTypes.STRING,
-        dance_style_description: DataTypes.STRING
-    },
-    {
-        freezeTableName: true,
-        tableName: "Ref_Dance_Style",
-        sequelize
-    })
+  {
+    dance_style: DataTypes.STRING,
+    dance_style_description: DataTypes.STRING
+  },
+  {
+    freezeTableName: true,
+    tableName: 'Ref_Dance_Style',
+    sequelize
+  }
+);
