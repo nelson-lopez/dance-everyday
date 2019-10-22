b6976ee7c48a58e6c889c730a653ffe4052961cc
Jaab <Jaabel@outlook.com>
2019-10-22 12:25:25 -0400

    route to edit event

--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ import express from 'express'
 import { Router } from 'express'
 import { Organizer, Event, Ref_Dance_Style, Venue } from './models'
 
+//-------- Gets all  -------
+
 const getAllEvents = async (
     /**@type {express.Request} */ req,
     /**@type {express.Response} */ res) => {
@@ -12,6 +14,8 @@ const getAllEvents = async (
             return res.status(500).send(error.message)
         }
     }
+
+//------- Gets by Primary Keys ------
 
 const getEventById = async (
     /**@type {express.Request} */ req,
@@ -24,6 +28,26 @@ const getEventById = async (
         }
     } 
 
+//-------- Updates entries ---------
+
+const updateEvent = async (
+    /**@type {express.Request} */ req,
+    /**@type {express.Response} */ res) => {
+        try{
+            let id = req.params.id
+            const editEvent = await Event.update(req.body, {
+                where: { id: id }
+            })
+            if(editEvent) {
+                let updated = await Event.findByPk(id)
+                return res.json(updated)
+            }
+        } catch(error){
+            return res.status(500).send(error.message)
+        }
+    }
+
 export const eventRouter = Router()
 eventRouter.get("/events", getAllEvents)
 eventRouter.get("/events/:id", getEventById)
+eventRouter.get("/events/:id/edit", updateEvent)
