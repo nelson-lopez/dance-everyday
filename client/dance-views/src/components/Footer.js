9ccf0fa9c93c43e832d3f39bf9893cf8a81019d2
Anna <annam.github@gmail.com>
2019-10-23 14:32:56 -0400

    created sticky footer

--- client/dance-views/src/components/Footer.js
+++ client/dance-views/src/components/Footer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
 export default function Footer() {
-  return <div>footer placeholder</div>;
+  return <div className="footer"> 
+    <p>© 2019 Dance Every Day. All rights reserved. Created by Anna Moser, Jaabel Lopez, and Nelson Lopez 🎵🎶</p>
+  </div>;
 }
