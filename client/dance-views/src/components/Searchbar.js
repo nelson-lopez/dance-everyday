cc1116856d9d78060cc4047529d094c01d964d85
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-10-23 10:06:13 -0400

    added dropdown toggle

--- client/dance-views/src/components/Searchbar.js
+++ client/dance-views/src/components/Searchbar.js
@@ -2,13 +2,29 @@ import React, { useState } from 'react';
 
 const Searchbar = () => {
   const [formInput, setInput] = useState('');
+  const [display, setDisplay] = useState(false);
 
   const handleOnInput = e => {
     setInput(e.target.value);
     console.log(formInput);
   };
+
+  const handleOnDisplay = () => {
+    setDisplay(!display);
+  };
   return (
     <div>
+      <button onClick={handleOnDisplay}>Search by type</button>
+      {display ? (
+        <div>
+          <button> Dance </button>
+          <button> Organizer </button>
+          <button> Date </button>
+        </div>
+      ) : (
+        <div> </div>
+      )}
+
       <form>
         <label>
           Search Event:
