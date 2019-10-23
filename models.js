6bc4cee6518e501b6e6435dcb6210fc9b073312a
Jaab <Jaabel@outlook.com>
2019-10-23 10:14:29 -0400

    added routes to vnues and orginizers

--- models.js
+++ models.js
@@ -14,11 +14,7 @@ const sequelize = new Sequelize({
 Organizer.init(
     {
         name: DataTypes.STRING,
-        company: DataTypes.STRING,
-        event_id: {
-            type: DataTypes.INTEGER,
-            foreignKey: true
-        }
+        company: DataTypes.STRING
     },
     {
         freezeTableName: true,
@@ -31,19 +27,7 @@ Event.init(
     {
         name: DataTypes.STRING,
         date: DataTypes.STRING,
-        description: DataTypes.STRING,
-        venue_id: {
-            type: DataTypes.INTEGER,
-            foreignKey: true
-        },
-        ref_dance_style_id: {
-            type: DataTypes.INTEGER,
-            foreignKey: true
-        },
-        event_id: {
-            type: DataTypes.INTEGER,
-            foreignKey: true
-        }
+        description: DataTypes.STRING
     },
     {
         freezeTableName: true,
@@ -56,11 +40,7 @@ Venue.init(
     {
         name: DataTypes.STRING,
         location: DataTypes.STRING,
-        contact: DataTypes.STRING,
-        event_id: {
-            type: DataTypes.INTEGER,
-            foreignKey: true
-        }
+        contact: DataTypes.STRING
     },
     {
         freezeTableName: true,
@@ -81,9 +61,9 @@ Ref_Dance_Style.init(
     }
 );
 
-Event.hasOne(Organizer, { sourceKey: "id", foreignKey: "event_id", keyType: DataTypes.INTEGER })
-Event.belongsTo(Venue, { sourceKey: "id", foreignKey: "event_id", keyType: DataTypes.INTEGER })
-// Event.hasMany(Ref_Dance_Style, { sourceKey: "id", foreignKey: "event_id", keyType: DataTypes.INTEGER })
-Venue.hasMany(Event, { sourceKey: "id", foreignKey: "venue_id", keyType: DataTypes.INTEGER })
-Organizer.hasMany(Event, { sourceKey: "id", foreignKey: "event_id", keyType: DataTypes.INTEGER })
-Ref_Dance_Style.hasMany(Event, { sourceKey: "id", foreignKey: "dance_style_id", keyType: DataTypes.INTEGER })
+Organizer.hasMany(Event)
+Event.belongsTo(Organizer)
+Venue.hasMany(Event)
+Event.belongsTo(Venue)
+Event.hasMany(Ref_Dance_Style)
+Ref_Dance_Style.hasMany(Event)
