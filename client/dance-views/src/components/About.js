5c16700f7523566e45e313a427134fa9329c9e82
Jaab <Jaabel@outlook.com>
2019-10-24 12:59:32 -0400

    about sections scafolding

--- client/dance-views/src/components/About.js
+++ client/dance-views/src/components/About.js
@@ -16,19 +16,19 @@ export default function About() {
         </div>
         <div className="met-your-devs">
           <h2>The Keyboard Hackers Who Made This For You</h2>
-          <div className="nelson">
+          <div className="dev">
             <h3 className="dev-name">Nelson Lopez</h3>
-            <img src="https://i.imgur.com/4lAzr1u.jpg" alt="Nelson Mondela"/>
+            <img className="profile" src="https://i.imgur.com/4lAzr1u.jpg" alt="Nelson Mondela"/>
             <h5 className="dev-about">The man, the coder, the legend. From professional bartender to senior software developer in the making. He truly believes in the tranformative power of simplicity and clear communication. Easily the most valuable player of the Dance Everyday team.</h5>
           </div>
-          <div className="anna">
+          <div className="dev">
             <h3 className="dev-name">Anna Moser</h3>
-            <img src="https://i.imgur.com/RTcpSKq.jpg" alt="Anna Moser"/>
+            <img className="profile" src="https://i.imgur.com/RTcpSKq.jpg" alt="Anna Moser"/>
             <h5 className="dev-about">Focused, creative, and a silent killer are short descriptions of her designing powers. She enforces that good design and good relationships come from collaboration. With her, our team stayed on track and overcame every bump in the road.</h5>
           </div>
-          <div className="jaabel">
+          <div className="dev">
             <h3 className="dev-name">Jaabel Lopez</h3>
-            <img src="https://i.imgur.com/BKbBbLq.jpg" alt="Jaabel Lopez"/>
+            <img className="profile" src="https://i.imgur.com/BKbBbLq.jpg" alt="Jaabel Lopez"/>
             <h5 className="dev-about">The author, the zouker, the challenger. Men with a vision are needed in every team, large or small. He an individual who brings his passions for creativity to the team and brings out the best in all the people he speaks to. As a team player, he elevates everyones experiences, engages his team, and inspires everyone everywhere.</h5>
           </div>
         </div>
