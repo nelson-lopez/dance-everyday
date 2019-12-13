1fc62574869c3600829d661fd5022b5dd6542649
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-12-12 20:49:57 -0500

    added rate limit middleware

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,6 +4,7 @@ import * as config from 'config';
 import { Logger } from '@nestjs/common';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
@@ -20,6 +21,12 @@ async function bootstrap() {
   });
   app.use(compression());
   app.use(helmet());
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000,
+      max: 100,
+    }),
+  );
 
   await app.listen(port);
 
