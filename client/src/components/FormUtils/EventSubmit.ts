d55f82bd48e317940146ee51685e88f5955eb5da
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-12-22 18:48:17 -0500

    added error handling for PATCH and POST

--- client/src/components/FormUtils/EventSubmit.ts
+++ client/src/components/FormUtils/EventSubmit.ts
@@ -10,7 +10,28 @@ export const OnEventSubmit = async (values: PostEvent): Promise<void> => {
     venueName: values.venueName
   })
     .then(res => console.log(res))
-    .catch(err => console.log(err));
+    .catch(err => {
+      if (err.response) {
+        /**
+         * * Server response outside of the range of 2xx
+         */
+        console.log(err.response.date);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        /**
+         * * No response was received so the error sits in the browser
+         */
+        console.log(err.request);
+      } else {
+        /**
+         * * Error setting up the request
+         */
+        console.log("Error", err.message);
+      }
+
+      console.log(err.config);
+    });
 
   return res;
 };
