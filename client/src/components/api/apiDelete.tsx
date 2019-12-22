7ccab3a8be5d316ddcecab6a82d9cb4541f61503
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-12-22 18:44:00 -0500

    added error handling for filter GET as well as comments

--- client/src/components/api/apiDelete.tsx
+++ client/src/components/api/apiDelete.tsx
@@ -6,12 +6,22 @@ const apiDelete = (id: number) => {
     .then(res => console.log(res))
     .catch(err => {
       if (err.response) {
+        /**
+         * * Server response outside of the range of 2xx
+         */
         console.log(err.response.data);
         console.log(err.response.status);
         console.log(err.response.headers);
       } else if (err.request) {
+        /**
+         * * No response was received so the error sits in the browser
+         */
+
         console.log(err.request);
       } else {
+        /**
+         * * Error setting up the request
+         */
         console.log("Error", err.message);
       }
       console.log(err.config);
--- client/src/components/api/useFilterSearch.tsx
+++ client/src/components/api/useFilterSearch.tsx
@@ -7,9 +7,32 @@ export const useFilterSearch = (searchTerm: string) => {
 
   useEffect(() => {
     if (searchTerm.length > 2) {
-      Axios.get(url).then(res => {
-        setData(res.data);
-      });
+      Axios.get(url)
+        .then(res => {
+          setData(res.data);
+        })
+        .catch(err => {
+          if (err.response) {
+            /**
+             * * Server response outside of the range of 2xx
+             */
+            console.log(err.response.date);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else if (err.request) {
+            /**
+             * * No response was received so the error sits in the browser
+             */
+            console.log(err.request);
+          } else {
+            /**
+             * * Error setting up the request
+             */
+            console.log("Error", err.message);
+          }
+
+          console.log(err.config);
+        });
     }
   }, [searchTerm, url]);
 
