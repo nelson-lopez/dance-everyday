f70b644e3ad44ed4a46d51e1fe3efcfbf09ac8f8
Nelson Lopez <dev.nelsonlopez@gmail.com>
2019-12-23 17:46:23 -0500

    working render on venue page

--- client/src/components/VenueCard.tsx
+++ client/src/components/VenueCard.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Venue } from "./types/venue.interfaces";
 
-const VenueCard = () => {
-  return <div></div>;
+const VenueCard = (props: Venue) => {
+  return (
+    <div className="card">
+      <img
+        src={
+          "https://images.pexels.com/photos/270837/pexels-photo-270837.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+        }
+        alt="test"
+      />
+
+      <h2>{props.name}</h2>
+      <h2>{props.location}</h2>
+      <p>{props.phone}</p>
+    </div>
+  );
 };
 
 export default VenueCard;
--- client/src/components/VenueList.tsx
+++ client/src/components/VenueList.tsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { venueApiGet } from "./api/venues/venue.apiGet";
 import { Venue } from "./types/venue.interfaces";
+import Nav from "./Nav";
+import Header from "./Header";
+import VenueCard from "./VenueCard";
 
 const VenueList = () => {
   const [venueList, setVenueList] = useState<Venue[] | null>(null);
@@ -9,7 +12,28 @@ const VenueList = () => {
     venueApiGet(setVenueList);
   }, []);
 
-  return <div></div>;
+  if (venueList) {
+    return (
+      <div>
+        <Header />
+        <Nav />
+        <div className="flex-container">
+          {venueList.map(venue => {
+            return (
+              <VenueCard
+                name={venue.name}
+                location={venue.location}
+                phone={venue.phone}
+                id={venue.id}
+                events={venue.events}
+                createdAt={venue.createdAt}
+              />
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else return <div> loading..</div>;
 };
 
 export default VenueList;
